7eb49413477c9a0a65073148a1cd678a9013c45b
Avamaco <Maco113002@gmail.com>
2022-11-20 18:34:07 +0100

    Poprawki zasugerowane przez Marcina + kilka komentarzy

--- lab_5_zad/motele.c
+++ lab_5_zad/motele.c
@@ -1,50 +1,47 @@
-//#define NDEBUG
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <assert.h>
 
 #define NOT_FOUND -1
 
 
-typedef struct motel{
+typedef struct motel {
     int num;    // który z kolei jest to motel
     int siec;   // do jakiej sieci należy
     int poz;    // odległość od początku autostrady
 } motel;
 
 // true jeśli sieci m1, m2, m3 są parami różne, false w przeciwnym przypadku
-bool czyRozneSieci(motel m1, motel m2, motel m3){
+bool czyRozneSieci(motel m1, motel m2, motel m3) {
     return (m1.siec != m2.siec && m2.siec != m3.siec && m3.siec != m1.siec);
 }
 
 
-void drukuj(motel m){
-    printf("Numer: %d  Sieć: %d  Pozycja: %d\n", m.num, m.siec, m.poz);
-    return;
-}
+// void drukuj(motel m) {
+//     printf("Numer: %d  Sieć: %d  Pozycja: %d\n", m.num, m.siec, m.poz);
+//     return;
+// }
 
 // nic nie robi. Jest po to by niepotrzebna zmienna nie wywoływała błędu.
-void nic(int x){
+void nic(int x) {
     x++;
     return;
 }
 
 
-int min(int a, int b){
+int min(int a, int b) {
     if (a < b) return a;
     return b;
 }
 
 
-int max(int a, int b){
+int max(int a, int b) {
     if (a > b) return a;
     return b;
 }
 
 // maksymalna odległość w trójce moteli
-int wieksza_odl(motel m1, motel m2, motel m3){
+int wieksza_odl(motel m1, motel m2, motel m3) {
     return max(abs(m2.poz - m1.poz), abs(m3.poz - m2.poz));
 }
 
@@ -52,11 +49,11 @@ int wieksza_odl(motel m1, motel m2, motel m3){
 // t - tablica przeszukiwanych moteli; n - liczba przeszukiwanych moteli; start - skąd zaczynamy szukać; inny - motel którego sieć już mamy.
 // Jeśli czy_malejaco = true, numer szukanych moteli maleje. Jeśli = false, numer szukanych moteli rośnie.
 // Zwraca {-1, -1, -1} jeśli nie udało się znaleźć.
-motel szukajInnego(motel * t, int n, motel start, motel inny, bool czy_malejaco){
+motel szukajInnego(motel * t, int n, motel start, motel inny, bool czy_malejaco) {
     int skok = czy_malejaco ? -1 : 1;
 
-    while (start.siec == inny.siec){
-            if ((start.num == n - 1 && !czy_malejaco)||(start.num == 0 && czy_malejaco)){    // nie udało się znaleźć motelu spełniającego wymagania
+    while (start.siec == inny.siec) {
+            if ((start.num == n - 1 && !czy_malejaco)||(start.num == 0 && czy_malejaco)) {    // nie udało się znaleźć motelu spełniającego wymagania
                 motel wynik = {NOT_FOUND, NOT_FOUND, NOT_FOUND};
                 return wynik;
             }
@@ -66,11 +63,11 @@ motel szukajInnego(motel * t, int n, motel start, motel inny, bool czy_malejaco)
 }
 
 // Tak jak szukajInnego(), ale mamy już 2 różne sieci. inny2 - drugi motel którego sieć już mamy
-motel szukajInnegoOd2(motel * motele, int n, motel start, motel inny, motel inny2, bool czy_malejaco){
+motel szukajInnegoOd2(motel * motele, int n, motel start, motel inny, motel inny2, bool czy_malejaco) {
     int skok = czy_malejaco ? -1 : 1;
 
-    while (!czyRozneSieci(start, inny, inny2)){
-            if ((start.num == n - 1 && !czy_malejaco)||(start.num == 0 && czy_malejaco)){    // nie udało się znaleźć motelu spełniającego wymagania
+    while (!czyRozneSieci(start, inny, inny2)) {
+            if ((start.num == n - 1 && !czy_malejaco)||(start.num == 0 && czy_malejaco)) {    // nie udało się znaleźć motelu spełniającego wymagania
                 motel wynik = {NOT_FOUND, NOT_FOUND, NOT_FOUND};
                 return wynik;
             }
@@ -80,7 +77,7 @@ motel szukajInnegoOd2(motel * motele, int n, motel start, motel inny, motel inny
 }
 
 
-int main(){
+int main() {
     int n;
     int no_errors;  // funkcja scanf zwraca wartość której nie potrzebuję, ale trzeba ją wykorzystać żeby nie było błędu
 
@@ -88,7 +85,7 @@ int main(){
     
     motel * motele = malloc((size_t)n * sizeof(motel));
 
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         no_errors = scanf("%d%d", &motele[i].siec, &motele[i].poz);
         motele[i].num = i;
     }
@@ -101,7 +98,7 @@ int main(){
 
     // niezmiennik: wszystkie motele między m1 a m2 są z tej samej sieci co m1
     m2 = szukajInnego(motele, n, m1, m1, false);
-    if (m2.num == NOT_FOUND){  // nie ma 3 roznych sieci
+    if (m2.num == NOT_FOUND) {  // nie ma 3 różnych sieci
         printf("0 0");
         free(motele);
         return 0;
@@ -111,7 +108,7 @@ int main(){
 
     // niezmiennik: wszystkie motele między m2 a m3 są z tej samej sieci co m1 lub m2
     m3 = szukajInnegoOd2(motele, n, m2, m1, m2, false);
-    if (m3.num == NOT_FOUND){  // nie ma 3 roznych sieci
+    if (m3.num == NOT_FOUND) {  // nie ma 3 różnych sieci
         printf("0 0");
         free(motele);
         return 0;
@@ -120,11 +117,12 @@ int main(){
     // minimum z maksimów odległości trzech moteli należących do różnych sieci
     int najblizsze = wieksza_odl(m1, m2, m3);
 
-    while (true){
+    // dla każdego motelu środkowego biorę najbliższy motel z innej sieci przed nim i najbliższy motel z innej sieci (niż oba) za nim
+    while (true) {
         motel oldm2 = m2;
         m2 = motele[m2.num + 1];
 
-        if (oldm2.siec != m2.siec){
+        if (oldm2.siec != m2.siec) {
             m1 = oldm2;
         }
 
@@ -139,22 +137,22 @@ int main(){
 
     printf("%d ", najblizsze);
 
+    // podczas szukania najdalszej trójki najpierw znajduję 3 różne motele maksymalnie na lewo i 3 maksymalnie na prawo
     motel lewe[3];
     lewe[0] = motele[0];
     lewe[1] = szukajInnego(motele, n, lewe[0], lewe[0], false);
     lewe[2] = szukajInnegoOd2(motele, n, lewe[1], lewe[0], lewe[1], false);
-    assert(lewe[1].num != NOT_FOUND && lewe[2].num != NOT_FOUND);    // już wiemy, że istnieją przynajmniej 3 sieci
 
     motel prawe[3];
     prawe[0] = motele[n - 1];
     prawe[1] = szukajInnego(motele, n, prawe[0], prawe[0], true);
     prawe[2] = szukajInnegoOd2(motele, n, prawe[1], prawe[0], prawe[1], true);
-    assert(prawe[1].num != NOT_FOUND && prawe[2].num != NOT_FOUND);    // już wiemy, że istnieją przynajmniej 3 sieci
 
     // maksimum z minimów odległości trzech moteli należących do różnych sieci
     int najdalsze = 0;
 
-    for (int i = 0; i < n; i++){
+    // dla każdego motelu znajduję motele z innych sieci maksymalnie na lewo i prawo
+    for (int i = 0; i < n; i++) {
         m2 = motele[i];
 
         if (lewe[0].siec != m2.siec) m1 = lewe[0];
