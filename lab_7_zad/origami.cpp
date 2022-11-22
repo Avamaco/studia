cd5162ae615690e7aedff1e70d579a0b4bf604e1
Avamaco <Maco113002@gmail.com>
2022-11-22 20:35:11 +0100

    Dodana funkcja odbicia punktu, debugging

--- lab_7_zad/origami.cpp
+++ lab_7_zad/origami.cpp
@@ -30,16 +30,31 @@ bool equals(double x, double y) {
 // zwraca po której stronie prostej l (przechodzącej przez l1 i l2) leży punkt p, patrząc z l1 na l2.
 // 1 - po lewej, 0 - na, -1 - po prawej
 int strona(punkt p, punkt l1, punkt l2) {
-    double p = (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x); // korzystam ze wzoru na iloczyn wektorowy
+    double w = (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x); // korzystam ze wzoru na iloczyn wektorowy
 
-    if (equals(p, 0)) return 0;
-    if (p > 0) return 1;
-    if (p < 0) return -1;
+    if (equals(w, 0)) return 0;
+    if (w > 0) return 1;
+    return -1;
 }
 
 // zwraca punkt będący odbiciem symetrycznym punktu p względem prostej l (przechodzącej przez l1 i l2).
 punkt odbicie(punkt p, punkt l1, punkt l2) {
-    // TODO
+    punkt spodek, wynik; // spodek - spodek wysokości opuszczonej z wierzchołka p trójkąta (l1 l2 p)
+
+    // a2, b2, c2 - kwadraty długości odcinków p-l1, l1-l2, l2-p
+    double a2 = (p.x - l1.x) * (p.x - l1.x) + (p.y - l1.y) * (p.y - l1.y);
+    double b2 = (l1.x - l2.x) * (l1.x - l2.x) + (l1.y - l2.y) * (l1.y - l2.y);
+    double c2 = (l2.x - p.x) * (l2.x - p.x) + (l2.y - p.y) * (l2.y - p.y);
+
+    // wzór na współrzędne spodka wysokości wyprowadziłem używając twierdzenia cosinusów
+    spodek.x = l1.x + (l2.x - l1.x) * (a2 + b2 - c2) / (2 * b2);
+    spodek.y = l1.y + (l2.y - l1.y) * (a2 + b2 - c2) / (2 * b2);
+
+    // wynik można uzyskać poprzez przesunięcie punktu spodek o wektor (p -> spodek)
+    wynik.x = 2 * spodek.x - p.x;
+    wynik.y = 2 * spodek.y - p.y;
+
+    return wynik;
 }
 
 // zwraca 1 punkt leży na prostokącie (włącznie z brzegiem), 0 w.p.p.
@@ -55,7 +70,7 @@ int czyNaProstokacie(punkt p, kartka a) {
 // zwraca 1 punkt leży na kole (włącznie z brzegiem), 0 w.p.p.
 int czyNaKole(punkt p, kartka a) {
     double kwadrat_odl = (p.x - a.p1.x) * (p.x - a.p1.x) + (p.y - a.p1.y) + (p.y - a.p1.y);
-    if (kwadrat_odl < r * r || equals(kwadrat_odl, r * r))
+    if (kwadrat_odl < a.r * a.r || equals(kwadrat_odl, a.r * a.r))
         return 1;
     return 0;
 }
@@ -63,21 +78,21 @@ int czyNaKole(punkt p, kartka a) {
 
 int main() {
     int n, q;
-    scanf("%d%d", &n, &q);
-    kartka * kartki = malloc((size_t)(n + 1) * sizeof(kartka));
+    scanf("%d %d", &n, &q);
+    kartka * kartki = (kartka *)malloc((n + 1) * sizeof(kartka));
 
     // wczytywanie opisów kartek
     for (int i = 1; i < n + 1; i++) {
         scanf("%c", &kartki[i].typ);
         switch (kartki[i].typ) {
           case 'P':
-            scanf("%f%f%f%f", &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].p2.x, &kartki[i].p2.y);
+            scanf("%lf %lf %lf %lf", &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].p2.x, &kartki[i].p2.y);
             break;
           case 'K':
-            scanf("%f%f%f", &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].r);
+            scanf("%lf %lf %lf", &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].r);
             break;
           case 'Z':
-            scanf("%d%f%f%f%f", &kartki[i].k, &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].p2.x, &kartki[i].p2.y);
+            scanf("%d %lf %lf %lf %lf", &kartki[i].k, &kartki[i].p1.x, &kartki[i].p1.y, &kartki[i].p2.x, &kartki[i].p2.y);
             break;
           default:
             break;
@@ -90,7 +105,7 @@ int main() {
         int k;
         punkt p;
 
-        scanf("%d%f%f", &k, &p.x, &p.y);
+        scanf("%d %lf %lf", &k, &p.x, &p.y);
         dziury.push(p);
 
         while (kartki[k].typ == 'Z') {
